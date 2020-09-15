c15ced961b1a724f8e3587e375c4732c9ef09a70
ramon <ramongduraes@gmail.com>
2020-09-15 11:39:30 -0300

    sharing second directory and renaming container to avoid confusion

--- run-container.sh
+++ run-container.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -p 8887:8887 --name covid_simulation --mount "type=bind,src=$(pwd),dst=/opt/singnet/covid-simulation" -ti covid-simulation bash
+docker run -p 8887:8887 --name covid_simulation_container --mount "type=bind,src=$(pwd),dst=/opt/singnet/covid-simulation" -v ~/Shared:/home/user/Shared -ti covid-simulation bash
