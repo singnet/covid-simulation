3ab5b2ae1fb8d4cebcacb7a35379d501b8741704
Andre Senna <andre.senna@gmail.com>
2020-10-02 09:34:38 -0300

    Update regression tests baseline

--- covid19_sir/regression-test.sh
+++ covid19_sir/regression-test.sh
@@ -3,6 +3,6 @@
 \rm -rf /tmp/regression-test/
 mkdir /tmp/regression-test/
 python3 simple-simulation.py 31415 &&\
-mv scenario[1234567].csv /tmp/regression-test/ &&\
-mv scenario[1234567].png /tmp/regression-test/ &&\
+mv scenario[123456].csv /tmp/regression-test/ &&\
+mv scenario[123456].png /tmp/regression-test/ &&\
 diff /tmp/regression-test/ regression-test-baseline/
