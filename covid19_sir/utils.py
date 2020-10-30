c8a01cd7e66fd6dfbd8d646af39a82896cae542b
ramon <ramongduraes@gmail.com>
2020-10-30 17:28:57 -0300

    adding support for multiple districts in setup_city_layout

--- covid19_sir/utils.py
+++ covid19_sir/utils.py
@@ -266,6 +266,24 @@ class RemovePolicy:
                 get_parameters().get('social_policies').remove(self.policy)
                 self.state = 1
 
+
+class AddPolicy:
+    def __init__(self, model, policy, day):
+        self.switch = day
+        self.policy = policy
+        self.model = model
+        self.state = 0
+
+    def start_cycle(self, model):
+        pass
+
+    def end_cycle(self, model):
+        if self.state == 0:
+            if self.model.global_count.day_count == self.switch:
+                get_parameters().get('social_policies').append(self.policy)
+                self.state = 1
+
+
 class AddPolicyInfectedRate:
     def __init__(self, model, policy, v):
         self.trigger = v
@@ -328,7 +346,7 @@ def build_district(name, model, population_size, building_capacity, unit_capacit
     return district
 
 
-def setup_city_layout(model, population_size):
+def setup_city_layout(model, population_size, num_districts=1):
     work_building_capacity = 20
     office_capacity = 10
     work_building_occupacy_rate = 0.5
@@ -338,139 +356,141 @@ def setup_city_layout(model, population_size):
     school_capacity = 50
     classroom_capacity = 20
     school_occupacy_rate = 0.5
+    district_population = int(np.floor(population_size/num_districts))
 
-    # Build empty districts
-    # https://docs.google.com/document/d/1imCNXOyoyecfD_sVNmKpmbWVB6xqP-FWlHELAyOg1Vs/edit
-    home_district = build_district("Home", model, population_size,
-                                   appartment_building_capacity,
-                                   appartment_capacity,
-                                   appartment_building_occupacy_rate,
-                                   beta_range(0.021, 0.12))  # normal_ci(0.021, 0.12, 10)
-    work_district = build_district("Work", model, population_size,
-                                   work_building_capacity,
-                                   office_capacity,
-                                   work_building_occupacy_rate,
-                                   beta_range(0.007, 0.06))  # normal_ci(0.007, 0.06, 10)
-    school_district = build_district("School", model, population_size,
-                                     school_capacity,
-                                     classroom_capacity,
-                                     school_occupacy_rate,
-                                     beta_range(0.014, 0.08))  # normal_ci(0.014, 0.08, 10)
+    for d in range(num_districts):
+        # Build empty districts
+        # https://docs.google.com/document/d/1imCNXOyoyecfD_sVNmKpmbWVB6xqP-FWlHELAyOg1Vs/edit
+        home_district = build_district("Home", model, district_population,
+                                       appartment_building_capacity,
+                                       appartment_capacity,
+                                       appartment_building_occupacy_rate,
+                                       beta_range(0.021, 0.12))  # normal_ci(0.021, 0.12, 10)
+        work_district = build_district("Work", model, district_population,
+                                       work_building_capacity,
+                                       office_capacity,
+                                       work_building_occupacy_rate,
+                                       beta_range(0.007, 0.06))  # normal_ci(0.007, 0.06, 10)
+        school_district = build_district("School", model, district_population,
+                                         school_capacity,
+                                         classroom_capacity,
+                                         school_occupacy_rate,
+                                         beta_range(0.014, 0.08))  # normal_ci(0.014, 0.08, 10)
 
-    home_district.debug = model.debug
-    work_district.debug = model.debug
-    school_district.debug = model.debug
+        home_district.debug = model.debug
+        work_district.debug = model.debug
+        school_district.debug = model.debug
 
-    # Add Restaurants to work_district
+        # Add Restaurants to work_district
 
-    for i in range(get_parameters().params['restaurant_count_per_work_district']):
-        if flip_coin(0.5):
-            restaurant_type = RestaurantType.FAST_FOOD
-            rtype = "FASTFOOD"
-        else:
-            restaurant_type = RestaurantType.FANCY
-            rtype = "FANCY"
-        restaurant = Restaurant(
-            normal_cap(
-                get_parameters().params['restaurant_capacity_mean'], 
-                get_parameters().params['restaurant_capacity_stdev'], 
-                16, 
-                200
-            ),
-            restaurant_type,
-            flip_coin(0.5),
-            model,
-            '',
-            rtype + '-' + str(i))
-        work_district.locations.append(restaurant)
-    for i in range(2):
-        bar = Restaurant(
-            normal_cap(100, 20, 50, 200),
-            RestaurantType.BAR,
-            flip_coin(0.5),
-            model,
-            '',
-            'BAR-' + str(i))
-        work_district.locations.append(bar)
+        for i in range(get_parameters().params['restaurant_count_per_work_district']):
+            if flip_coin(0.5):
+                restaurant_type = RestaurantType.FAST_FOOD
+                rtype = "FASTFOOD"
+            else:
+                restaurant_type = RestaurantType.FANCY
+                rtype = "FANCY"
+            restaurant = Restaurant(
+                normal_cap(
+                    get_parameters().params['restaurant_capacity_mean'],
+                    get_parameters().params['restaurant_capacity_stdev'],
+                    16,
+                    200
+                ),
+                restaurant_type,
+                flip_coin(0.5),
+                model,
+                '',
+                rtype + '-' + str(i))
+            work_district.locations.append(restaurant)
+        for i in range(2):
+            bar = Restaurant(
+                normal_cap(100, 20, 50, 200),
+                RestaurantType.BAR,
+                flip_coin(0.5),
+                model,
+                '',
+                'BAR-' + str(i))
+            work_district.locations.append(bar)
 
-    # print(home_district)
-    # print(work_district)
-    # print(school_district)
+        # print(home_district)
+        # print(work_district)
+        # print(school_district)
 
-    # Build families
+        # Build families
 
-    family_factory = FamilyFactory(model)
-    family_factory.factory(population_size)
-    model.global_count.total_population = family_factory.human_count
+        family_factory = FamilyFactory(model)
+        family_factory.factory(district_population)
+        model.global_count.total_population = family_factory.human_count
 
-    # print(family_factory)
+        # print(family_factory)
 
-    age_group_sets = {
-        Infant: [],
-        Toddler: [],
-        K12Student: [],
-        Adult: [],
-        Elder: []
-    }
+        age_group_sets = {
+            Infant: [],
+            Toddler: [],
+            K12Student: [],
+            Adult: [],
+            Elder: []
+        }
 
-    # Allocate buildings to people
+        # Allocate buildings to people
 
-    all_adults = []
-    all_students = []
-    for family in family_factory.families:
-        adults = [human for human in family if isinstance(human, Adult)]
-        students = [human for human in family if isinstance(human, K12Student)]
-        home_district.allocate(family, True, True, True)
-        work_district.allocate(adults)
-        school_district.allocate(students, True)
-        for human in family:
-            age_group_sets[type(human)].append(human)
-            human.home_district = home_district
-            home_district.get_buildings(human)[0].get_unit(human).humans.append(human)
-        for adult in adults:
-            adult.work_district = work_district
-            all_adults.append(adult)
-        for student in students:
-            student.school_district = school_district
-            all_students.append(student)
+        all_adults = []
+        all_students = []
+        for family in family_factory.families:
+            adults = [human for human in family if isinstance(human, Adult)]
+            students = [human for human in family if isinstance(human, K12Student)]
+            home_district.allocate(family, True, True, True)
+            work_district.allocate(adults)
+            school_district.allocate(students, True)
+            for human in family:
+                age_group_sets[type(human)].append(human)
+                human.home_district = home_district
+                home_district.get_buildings(human)[0].get_unit(human).humans.append(human)
+            for adult in adults:
+                adult.work_district = work_district
+                all_adults.append(adult)
+            for student in students:
+                student.school_district = school_district
+                all_students.append(student)
 
-    # Set tribes
+        # Set tribes
 
-    adult_rf = HomophilyRelationshipFactory(model, all_adults)
-    student_rf = HomophilyRelationshipFactory(model, all_students)
-    # exit()
+        adult_rf = HomophilyRelationshipFactory(model, all_adults)
+        student_rf = HomophilyRelationshipFactory(model, all_students)
+        # exit()
 
-    count = 0
-    for family in family_factory.families:
-        for human in family:
-            count += 1
-            human.tribe[TribeSelector.AGE_GROUP] = age_group_sets[type(human)]
-            human.tribe[TribeSelector.FAMILY] = family
-            if isinstance(human, Adult):
-                human.unique_id = "Adult" + str(count)
-                human.tribe[TribeSelector.COWORKER] = work_district.get_buildings(human)[0].get_unit(human).allocation
-                t1 = adult_rf.build_tribe(human, human.tribe[TribeSelector.COWORKER], 1, office_capacity)
-                t2 = adult_rf.build_tribe(human, human.tribe[TribeSelector.AGE_GROUP], 1, 20)
-                human.tribe[TribeSelector.FRIEND] = t1
-                for h in t2:
-                    if h not in human.tribe[TribeSelector.FRIEND]:
-                        human.tribe[TribeSelector.FRIEND].append(h)
-            elif isinstance(human, K12Student):
-                human.unique_id = "K12Student" + str(count)
-                human.tribe[TribeSelector.CLASSMATE] = school_district.get_buildings(human)[0].get_unit(
-                    human).allocation
-                t1 = student_rf.build_tribe(human, human.tribe[TribeSelector.CLASSMATE], 1, classroom_capacity)
-                t2 = student_rf.build_tribe(human, human.tribe[TribeSelector.AGE_GROUP], 1, 20)
-                human.tribe[TribeSelector.FRIEND] = t1
-                for h in t2:
-                    if h not in human.tribe[TribeSelector.FRIEND]:
-                        human.tribe[TribeSelector.FRIEND].append(h)
-            elif isinstance(human, Elder):
-                human.unique_id = "Elder" + str(count)
-            elif isinstance(human, Infant):
-                human.unique_id = "Infant" + str(count)
-            elif isinstance(human, Toddler):
-                human.unique_id = "Toddler" + str(count)
+        count = 0
+        for family in family_factory.families:
+            for human in family:
+                count += 1
+                human.tribe[TribeSelector.AGE_GROUP] = age_group_sets[type(human)]
+                human.tribe[TribeSelector.FAMILY] = family
+                if isinstance(human, Adult):
+                    human.unique_id = "Adult" + str(count)
+                    human.tribe[TribeSelector.COWORKER] = work_district.get_buildings(human)[0].get_unit(human).allocation
+                    t1 = adult_rf.build_tribe(human, human.tribe[TribeSelector.COWORKER], 1, office_capacity)
+                    t2 = adult_rf.build_tribe(human, human.tribe[TribeSelector.AGE_GROUP], 1, 20)
+                    human.tribe[TribeSelector.FRIEND] = t1
+                    for h in t2:
+                        if h not in human.tribe[TribeSelector.FRIEND]:
+                            human.tribe[TribeSelector.FRIEND].append(h)
+                elif isinstance(human, K12Student):
+                    human.unique_id = "K12Student" + str(count)
+                    human.tribe[TribeSelector.CLASSMATE] = school_district.get_buildings(human)[0].get_unit(
+                        human).allocation
+                    t1 = student_rf.build_tribe(human, human.tribe[TribeSelector.CLASSMATE], 1, classroom_capacity)
+                    t2 = student_rf.build_tribe(human, human.tribe[TribeSelector.AGE_GROUP], 1, 20)
+                    human.tribe[TribeSelector.FRIEND] = t1
+                    for h in t2:
+                        if h not in human.tribe[TribeSelector.FRIEND]:
+                            human.tribe[TribeSelector.FRIEND].append(h)
+                elif isinstance(human, Elder):
+                    human.unique_id = "Elder" + str(count)
+                elif isinstance(human, Infant):
+                    human.unique_id = "Infant" + str(count)
+                elif isinstance(human, Toddler):
+                    human.unique_id = "Toddler" + str(count)
 
     # print(home_district)
     # print(work_district)
