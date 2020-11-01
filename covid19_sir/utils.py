baf228693482ec14d46d19ec552f07439993f89f
Deborah Duong <deborahvakasduong@gmail.com>
2020-11-01 22:04:19 +0100

    Allocate relations according to locality using a grid

--- covid19_sir/utils.py
+++ covid19_sir/utils.py
@@ -5,7 +5,8 @@ from model.human import Elder, Adult, K12Student, Toddler, Infant
 from model.location import District, HomogeneousBuilding, BuildingUnit, Restaurant
 from model.instantiation import FamilyFactory, HomophilyRelationshipFactory
 from model.utils import TribeSelector, RestaurantType
-
+import model.utils
+import copy
 from scipy.stats import sem, t
 import random
 import math
@@ -26,8 +27,9 @@ def confidence_interval(data, confidence=0.95):
 
 
 def multiple_runs(params, population_size, simulation_cycles, num_runs=5, seeds=None, debug=False,
-                  desired_stats=None,fname="scenario",
-                  do_print=False):
+                  desired_stats=None,fname="scenario", listeners = [],do_print=False, 
+                  home_grid_height = 3, home_grid_width = 4, 
+                  work_height = 1, work_width =2, school_height=2, school_width=1):
     color = {
             'susceptible': 'lightblue',
             'infected': 'gray',
@@ -60,13 +62,25 @@ def multiple_runs(params, population_size, simulation_cycles, num_runs=5, seeds=
         upper[stat] = []
         lower[stat]= []
     for s in randomlist:
-        set_parameters(params)
+        paramcopy = copy.deepcopy(params)
+        set_parameters(paramcopy)
         model = CovidModel(debug=debug)
+        l = []
+        ls= copy.deepcopy(listeners)
+        for listener in ls:
+            funct = listener.pop(0)
+            listener[:0]=[model]
+            l.append(globals()[funct](*listener))
+        for k in l:
+            model.add_listener(k)
+        #for m in model.ls:
+            #print(m)
         if debug:
             model.debug_each_n_cycles = 20
         np.random.seed(s + 1)
         random.seed(s + 2)
-        setup_city_layout(model, population_size)
+        setup_grid_layout(model, population_size, home_grid_height, 
+        home_grid_width,work_height,work_width, school_height, school_width)
         if do_print:
             print("run with seed {0}:".format(str(s)))
         model.reset_randomizer(s)
@@ -328,6 +342,7 @@ def build_district(name, model, population_size, building_capacity, unit_capacit
     return district
 
 
+
 def setup_city_layout(model, population_size):
     work_building_capacity = 20
     office_capacity = 10
@@ -448,6 +463,232 @@ def setup_city_layout(model, population_size):
             human.tribe[TribeSelector.FAMILY] = family
             if isinstance(human, Adult):
                 human.unique_id = "Adult" + str(count)
+                human.tribe[TribeSelector.COWORKER] = work_district.get_buildings(human)[0].get_unit(human).allocation
+                t1 = adult_rf.build_tribe(human, human.tribe[TribeSelector.COWORKER], 1, office_capacity)
+                t2 = adult_rf.build_tribe(human, human.tribe[TribeSelector.AGE_GROUP], 1, 20)
+                human.tribe[TribeSelector.FRIEND] = t1
+                for h in t2:
+                    if h not in human.tribe[TribeSelector.FRIEND]:
+                        human.tribe[TribeSelector.FRIEND].append(h)
+            elif isinstance(human, K12Student):
+                human.unique_id = "K12Student" + str(count)
+                human.tribe[TribeSelector.CLASSMATE] = school_district.get_buildings(human)[0].get_unit(
+                    human).allocation
+                t1 = student_rf.build_tribe(human, human.tribe[TribeSelector.CLASSMATE], 1, classroom_capacity)
+                t2 = student_rf.build_tribe(human, human.tribe[TribeSelector.AGE_GROUP], 1, 20)
+                human.tribe[TribeSelector.FRIEND] = t1
+                for h in t2:
+                    if h not in human.tribe[TribeSelector.FRIEND]:
+                        human.tribe[TribeSelector.FRIEND].append(h)
+            elif isinstance(human, Elder):
+                human.unique_id = "Elder" + str(count)
+            elif isinstance(human, Infant):
+                human.unique_id = "Infant" + str(count)
+            elif isinstance(human, Toddler):
+                human.unique_id = "Toddler" + str(count)
+
+def setup_grid_layout(model, population_size, 
+        home_grid_height, home_grid_width,work_height,work_width, school_height, school_width):
+    #Makes a grid of homogeneous home districts, overlaid by school and work districts.
+    #home_grid_height is the number of home districts high the grid is, and 
+    #home_grid_width is the nmber of home districts wide the grid is
+    #school height and work height are how many home districts high a school
+    #district and work are respectively, and the same for their length.
+    #each begins in grid 0,0 and cover the orignal home district grid.  
+    #Persons assigned to the home districts are also assigned to the school
+    #and work districts that cover them. The parameters determine the amount
+    #of leakage across groups of people.  With parameters (10,10,1,1,1,1) you get 100
+    #completely separated districts with no leakage.  With parameters (3,4,1,2,2,1) you 
+    #get a grid where every one is connected to everyone else, but there is a large 
+    #degree of separation.  For example, a person in home district (0,0) can be infected
+    #by a person in (2,3) but it would be bridged by five infections, slowing the 
+    #virus down.  Larger sizes for work and school districts enable faster spread. Fastest
+    #spread occurs with parameters (1,1,1,1,1,1) or equivalently (10,10, 10,10,10,10)
+    #or any of the same number
+    #Since this is just a way to allocate human interactions, no label is needed and 
+    #the grid need not be saved, for interactions to occur, although this inforamtion
+    #may be useful for visualizations.
+    
+    work_building_capacity = 20
+    office_capacity = 10
+    work_building_occupacy_rate = 0.5
+    appartment_building_capacity = 20
+    appartment_capacity = 5
+    appartment_building_occupacy_rate = 0.5
+    school_capacity = 50
+    classroom_capacity = 20
+    school_occupacy_rate = 0.5
+
+    # Build empty districts
+    # https://docs.google.com/document/d/1imCNXOyoyecfD_sVNmKpmbWVB6xqP-FWlHELAyOg1Vs/edit
+
+    home_districts = []
+    work_districts=[]
+    school_districts = []
+    school_map ={}
+    work_map = {}
+    school_grid_height = math.ceil(home_grid_height/school_height)
+    school_grid_width = math.ceil(home_grid_width/school_width)
+    work_grid_height = math.ceil(home_grid_height/work_height)
+    work_grid_width = math.ceil(home_grid_width/work_width)
+
+    for hw in range(home_grid_width):
+        for hh in range(home_grid_height):
+
+            home_district = build_district(f"Home ({hh},{hw})", model, population_size,
+                                   appartment_building_capacity,
+                                   appartment_capacity,
+                                   appartment_building_occupacy_rate,
+                                   beta_range(0.021, 0.12))  # normal_ci(0.021, 0.12, 10)
+
+            home_district.debug = model.debug
+
+            home_districts.append(home_district)
+            home_number = hw*home_grid_height + hh 
+            assert home_number == len(home_districts) - 1
+
+            sh = hh // school_height 
+            sw = hw // school_width
+            school_number = sw*school_grid_height+ sh
+            school_map[home_number] = school_number
+
+            wh = hh // work_height 
+            ww = hw // work_width
+            work_number = ww*work_grid_height+ wh
+            work_map[home_number] = work_number
+
+
+    for ww in range(work_grid_width):
+        for wh in range(work_grid_height):
+             
+            work_district = build_district(f"Work ({wh},{ww})", model, population_size,
+                                   work_building_capacity,
+                                   office_capacity,
+                                   work_building_occupacy_rate,
+                                   beta_range(0.007, 0.06))  # normal_ci(0.007, 0.06, 10)
+            # Add Restaurants to work_district
+
+            for i in range(get_parameters().params['restaurant_count_per_work_district']):
+                if flip_coin(0.5):
+                    restaurant_type = RestaurantType.FAST_FOOD
+                    rtype = "FASTFOOD"
+                else:
+                    restaurant_type = RestaurantType.FANCY
+                    rtype = "FANCY"
+                restaurant = Restaurant(
+                    normal_cap(
+                        get_parameters().params['restaurant_capacity_mean'], 
+                        get_parameters().params['restaurant_capacity_stdev'], 
+                        16, 
+                        200
+                    ),
+                    restaurant_type,
+                    flip_coin(0.5),
+                    model,
+                    '',
+                    rtype + '-' + str(i)+ f"({wh},{ww})")
+                work_district.locations.append(restaurant)
+            for i in range(2):
+                bar = Restaurant(
+                    normal_cap(100, 20, 50, 200),
+                    RestaurantType.BAR,
+                    flip_coin(0.5),
+                    model,
+                    '',
+                    'BAR-' + str(i)+ f"({wh},{ww})")
+                work_district.locations.append(bar)
+            work_district.debug = model.debug
+            work_districts.append(work_district)
+
+
+    for sw in range(school_grid_width):
+        for sh in range(school_grid_height):
+    
+            school_district = build_district(f"School ({sh},{sw})", model, population_size,
+                                     school_capacity,
+                                     classroom_capacity,
+                                     school_occupacy_rate,
+                                     beta_range(0.014, 0.08))  # normal_ci(0.014, 0.08, 10)
+            
+            school_district.debug = model.debug
+            school_districts.append(school_district)
+
+    #print ("work_map")
+    #print (work_map)
+    #print ("school_map")
+    #print (school_map)
+    
+
+    # Build families
+
+    family_factory = FamilyFactory(model)
+    family_factory.factory(population_size)
+    model.global_count.total_population = family_factory.human_count
+
+    # print(family_factory)
+
+    age_group_sets = {
+        Infant: [],
+        Toddler: [],
+        K12Student: [],
+        Adult: [],
+        Elder: []
+    }
+
+    # Allocate buildings to people
+    #print ("home_districts")
+    #print (home_districts)
+    #print ("work_districts")
+    #print (work_districts)
+    #print ("school_districts")
+    #print (school_districts)
+
+    all_adults = []
+    all_students = []
+    for family in family_factory.families:
+        adults = [human for human in family if isinstance(human, Adult)]
+        students = [human for human in family if isinstance(human, K12Student)]
+        home_district_num = np.random.randint (0,len(home_districts))
+        #print("home_district_num")
+        #print(home_district_num)
+        home_district = home_districts[home_district_num]
+        work_district = work_districts[work_map[home_district_num]]
+        school_district = school_districts[school_map[home_district_num]]
+    
+        home_district.allocate(family, True, True, True)
+        work_district.allocate(adults)
+        school_district.allocate(students, True)
+        for human in family:
+            age_group_sets[type(human)].append(human)
+            human.home_district = home_district
+            home_district.get_buildings(human)[0].get_unit(human).humans.append(human)
+        for adult in adults:
+            adult.work_district = work_district
+            all_adults.append(adult)
+        for student in students:
+            student.school_district = school_district
+            all_students.append(student)
+
+    # Set tribes
+
+    adult_rf = HomophilyRelationshipFactory(model, all_adults)
+    student_rf = HomophilyRelationshipFactory(model, all_students)
+    # exit()
+
+    count = 0
+    for family in family_factory.families:
+        for human in family:
+            work_district = human.work_district
+            school_district = human.school_district
+            count += 1
+            human.tribe[TribeSelector.AGE_GROUP] = age_group_sets[type(human)]
+            human.tribe[TribeSelector.FAMILY] = family
+            if isinstance(human, Adult):
+                human.unique_id = "Adult" + str(count)
+                #print(work_district.get_buildings(human))
+                #print(work_district.get_buildings(human))
+                #print(workd_district.get_buildings(human)[0].get_unit(human))
+                #print(workd_district.get_buildings(human)[0].get_unit(human))
                 human.tribe[TribeSelector.COWORKER] = work_district.get_buildings(human)[0].get_unit(human).allocation
                 t1 = adult_rf.build_tribe(human, human.tribe[TribeSelector.COWORKER], 1, office_capacity)
                 t2 = adult_rf.build_tribe(human, human.tribe[TribeSelector.AGE_GROUP], 1, 20)
